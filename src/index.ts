026853a0fa0ea02400511318a0b7ec0df48e252f
maamokun <sachadeco214@gmail.com>
2025-10-24 00:20:49 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const allowedDomains = process.env.ALLOWED_DOMAINS
+  ? process.env.ALLOWED_DOMAINS.split(",")
+  : [];
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const defaultRedirect = process.env.DEFAULT_REDIRECT || "";
+
+const ImgproxyURL = process.env.IMGPROXY_URL || "";
+
+const app = new Elysia();
+
+app.get('/', ({ redirect }) => {
+    return redirect(defaultRedirect);
+})
+
+app.get('/:path', async ({ query, params }) => {
+    const size = query.size;
+    const quality = query.quality;
+    const url = params.path;
+
+    const urlObj = new URL(url);
+
+    if (!allowedDomains.includes(urlObj.hostname)) {
+        return new Response("Domain not allowed", { status: 403 });
+    }
+
+    let imgproxyPath = `/pr:sharp/resize:fill:${size}/q:${quality}/plain/${encodeURIComponent(url)}`;
+    const imgproxyUrl = new URL(ImgproxyURL + imgproxyPath);
+
+    const data = await fetch(imgproxyUrl.href);
+    return new Response(data.body, {
+        headers: {
+            'Content-Type': data.headers.get('Content-Type') || 'image/jpeg'
+        }
+    });
+})
+
+export default app;
